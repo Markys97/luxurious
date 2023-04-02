85c88f60d49b0b73010bf7e525a385c3e5accfbc
brexe junior <brexejunior@gmail.com>
2023-04-02 20:47:46 +0300

    end with filtre

--- src/component/ui/Trie/Trie.jsx
+++ src/component/ui/Trie/Trie.jsx
@@ -13,7 +13,6 @@ function Trie() {
     const category = useSelector(state=>state.product.category)
     const currentLang = useSelector(state=> state.setting.lang.value)
     
-
     const getListProductToShow = (products,activeItemTrieId,listTrie,category)=>{
         let activeItemCategory = category
         .filter(item => {
@@ -42,18 +41,33 @@ function Trie() {
         
 
         if(newListproduct.length!==0){
-            dispatch(setProductToShow(newListproduct))
+           
+            dispatch(setProductToShow( sortproduct(newListproduct,activeItemTrie)))
         }else{
-            dispatch(setProductToShow(products))
+            dispatch(setProductToShow( sortproduct([...products],activeItemTrie)))
         }
        
 
 
     }
 
+    const sortproduct = (products,indexActiveTrie) =>{
+        let productList=[...products]
+        switch(indexActiveTrie){
+            case 1:
+                return productList.sort((a,b) => a.price - b.price );
+            case 2:
+                return productList.sort((a,b) => b.price - a.price );
+            case 3:
+                return  productList.sort((a,b) => a.name - b.name );
+            default :
+            return 'default'
+        }
+    }
  
-
     getListProductToShow(products,activeItemTrie,itemTrieListLang,category)
+
+    
 
     const getCurrentItemTrieValue = (activeItemTrie,itemTrieListLang) =>(
         itemTrieListLang.find(item => item.id === activeItemTrie
@@ -64,13 +78,12 @@ function Trie() {
     }
 
     const changecurrentTrie = (id) => {
-        // seTcurrentTrieItem(id);
         dispatch(setActiveItemTrie(id))
         setIsOpenBodyTrie(false);
     }
 
     let bodyListTrieItemLang = getNoActiveItemTrieLang(itemTrieListLang)
-     const currentItemTrieValue = getCurrentItemTrieValue(activeItemTrie,itemTrieListLang)
+    const currentItemTrieValue = getCurrentItemTrieValue(activeItemTrie,itemTrieListLang)
      
 
 
@@ -79,7 +92,6 @@ function Trie() {
             <div className="trie__content">
                 <div onClick={()=> setIsOpenBodyTrie(prev=> !prev)} className="trie__head">
                     <div className="trie__title">
-
                         <div className={`trie__title-icon ${isOpenBodyTrie?' trie-body-open':''}`}>
                             <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z" fill="#2C2C2C"/>
@@ -89,7 +101,6 @@ function Trie() {
                         <div className="trie__title-text">
                           {`${textTrieLang.title[currentLang]}:`}
                         </div>
-
                     </div>
 
                     <div className="trie__currentValue">
