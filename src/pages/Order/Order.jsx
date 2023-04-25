399cfac6ae6ebadb36ebe5650586c0781fb2a6ef
brexe junior <brexejunior@gmail.com>
2023-04-25 23:07:54 +0300

    25.04.23 end

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -14,10 +14,14 @@ function Order() {
     const {id} =useParams()
     const baseUrlApi = useSelector( state => state.setting.baseUrlApi)
     let baseUrlSrcImg =`${baseUrlApi}/product/`;
+    let imgRef= useRef();
     
-    const {name,genre,price,description} = product
-    console.log(product.allImg,'mama')
+    let {name,genre,price,description,size} = product
+    let state;
 
+    if(product.state !== undefined){
+        state= JSON.parse(product.state)
+    }
     
 
 
@@ -38,14 +42,40 @@ function Order() {
                 // product.description = JSON.parse(description)
                 let allImgs = [JSON.parse(preview),...JSON.parse(imgs)];
                 product.allImg =allImgs
-
-                console.log(product.description,'mamam')
                 setProduct(product)
             }
         })
 
       
     },[])
+
+    let genreText = {
+        M:{
+            fr:'chaussure pour homme',
+            en:"men's shoes",
+            ru:"мужская обувь"
+        },
+        F:{
+            fr:'chaussure pour femme',
+            en:"Women's shoes",
+            ru:"Женская обувь"
+        },
+        K:{
+            fr:'chaussure pour enfant',
+            en:"kid's shoes",
+            ru:"детская обувь"
+        }
+    }
+
+
+    if(imgRef.value !== undefined){
+        console.log(imgRef.value,'tata')
+    }
+
+    const changeSrc = (e,imgSrc) =>{
+        e.target.src= imgSrc;
+        e.target.closest('.order__sneaker-img').classList.remove('loading')
+    }
 
 
 
@@ -60,21 +90,28 @@ function Order() {
                         <div className="order__left">
                             <div className="sneaker-info">
                                 <div className="sneaker-info__promo">
-                                    Member Access
+                                    {(state!== undefined) && state.news[currentLang]}
                                 </div>
                                 <div className="sneaker-info__name">
                                    {name}
                                 </div>
-                                <div className="sneaker-info__genre">men's Road Running Shows</div>
+                                <div className="sneaker-info__genre">
+                                    {
+                                         (genre !== undefined) && (
+                                            genreText[genre][currentLang]
+                                         )
+                                    }
+                                </div>
                             </div>
                             <div className="order__scroll-grid">
                                 <div className="order__grid">
                                     
                                     {
                                         
-                                        product.allImg && product.allImg.map((productImg,index)=> (
-                                            <div key={index} className="order__sneaker-img ">
-                                                <img src={`${baseUrlSrcImg}/${productImg}`} alt="sneaker" />
+                                        (product.allImg !== undefined) && product.allImg.map((productImg,index)=> (
+                                            <div key={index} className="order__sneaker-img loading">
+                                                {/* <img src={`${baseUrlSrcImg}/${productImg}`} alt="sneaker" /> */}
+                                                <img onLoad={(e)=>changeSrc(e,`${baseUrlSrcImg}/${productImg}`)} src={imgLoadingSrc} alt="" ref={imgRef}/>
                                             </div>
                                         ))
                                     }
@@ -84,56 +121,43 @@ function Order() {
                         <div className="order__right ">
                             <div className="sneaker-info">
                                 <div className="sneaker-info__promo">
-                                    Member Access
+                                {(state!== undefined) && state.news[currentLang]}
                                 </div>
                                 <div className="sneaker-info__name">
-                                    {name}
+                                    {(name !== undefined) && name}
                                 </div>
-                                <div className="sneaker-info__genre">men's Road Running Shows</div>
+                                <div className="sneaker-info__genre">
+                                    {
+                                            (genre !== undefined) && (
+                                            genreText[genre][currentLang]
+                                            )
+                                    }
+                                </div>
                                 <div className="sneaker-info__price">
-                                    <div className="sneaker-info__price--old">100 $</div>
-                                    <div className="sneaker-info__price--new"> 200 $</div>
+                                    <div className="sneaker-info__price--new">
+                                            {
+                                                (state!== undefined) && (
+                                                    (state.solde.active)? (
+                                                    ( price - price*state.solde.percent/100) +' ₽'
+                                                    ):
+                                                    (price)
+                                                )
+                                            }
+                                    </div>
+                                    {
+                                        (state!== undefined && state.solde.active) && (
+                                            <div className="sneaker-info__price--old">
+                                                {(price!== undefined) && price +'₽'} 
+                                            </div>
+                                        )
+                                    }
+                                   
+                                    
                                 </div>
                             </div>
                             <div className="sneaker-preview">
                                 <div className="sneaker-preview__scroll">
                                     <div className="sneaker-preview__row">
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
-                                        <div className="sneaker-preview__img">
-                                            <img src="/images/product/02.jpg" alt="preview"/>
-                                        </div>
                                         <div className="sneaker-preview__img">
                                             <img src="/images/product/02.jpg" alt="preview"/>
                                         </div>
@@ -143,18 +167,17 @@ function Order() {
                             <div className="sneaker-size">
                                 <h3 className="sneaker-size__title">Select Size</h3>
                                 <div className="sneaker-size__row">
-                                    <div className="sneaker-size__item">
-                                        45
-                                    </div>
-                                    <div className="sneaker-size__item">
-                                        45
-                                    </div>
-                                    <div className="sneaker-size__item">
-                                        45
-                                    </div>
-                                    <div className="sneaker-size__item">
-                                        45
-                                    </div>
+                                   
+                                    {
+                                        (size !== undefined) && (
+                                            JSON.parse(size).map((itemSize,index)=>(
+                                            <div key={index} className="sneaker-size__item">
+                                               {itemSize}
+                                            </div>
+                                            ))
+                                        )
+                                    }
+                                    
                                 </div>
                             </div>
         
